2c371bcd6da3830dba56a2649f517658733b0a34
Fariah Azam <fariaha124@gmail.com>
2023-09-23 18:08:29 -0400

    created student class and student runner class

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,22 @@
 public class Student {
+    private double gpaa;
+    private int age;
+    private int grade;
+    private String namee;
+
+
+    public Student(String name, int age, int grade, double gpa) {
+        namee = name;
+        gpaa = gpa;
+        System.out.println("This student's name is " + name + ". They are " + age + " years old.");
+        System.out.println("They are in grade " + grade + ". Their GPA is " + gpa);
+    }
+
+    public void honors() {
+        if(gpaa>85) {
+            System.out.println("Their GPA is " + gpaa + ". " + namee + " is an honors student.");
+        } else  {
+            System.out.println("Their GPA is " + gpaa + ". " + namee + " is not an honors student.");
+        }
+    }
 }
--- src/StudentRunner.java
+++ src/StudentRunner.java
@@ -1,5 +1,6 @@
 public class StudentRunner {
     public static void main(String[] args) {
-
+        Student student1 =  new Student("Amelia", 16, 11, 97);
+        student1.honors();
     }
 }
